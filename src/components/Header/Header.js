695a44d3470899d1a366545c0cf11e6fcdffa452
Aurélien PERNET <aurelien.pernet@ekium.eu>
2026-06-11 15:05:27 +0200

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,27 @@
+import { Roboto } from "next/font/google";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+
+const roboto = Roboto({ subsets: ["latin"], weight: ["100", "300"] });
+
+export default function Header() {
+  return (
+    <header className={`${roboto.className}`}>
+      <div>
+        <p>MyMovieApp</p>
+      </div>
+      <div>
+        <nav>
+          <ul>
+            <li>Séries</li>
+            <li>Films</li>
+          </ul>
+        </nav>
+      </div>
+      <input type="text" placeholder="Rechercher un titre ..." />
+      <div>
+        <FontAwesomeIcon icon={faUser} />
+      </div>
+    </header>
+  );
+}
